ca4c5957736c58f43be39842ac50c4063e5cfaa5
IsaacBevis123 <isaacbevis123@gmail.com>
2022-10-27 14:01:48 -0400

    Add gtest framework ref #12

---
+++ code/rollingCodeTests/build/CMakeFiles/rollingCodeTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rollingCodeTests.dir/rollingCodeTests.cpp.o"
+  "CMakeFiles/rollingCodeTests.dir/rollingCodeTests.cpp.o.d"
+  "rollingCodeTests"
+  "rollingCodeTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rollingCodeTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
